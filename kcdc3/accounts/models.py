b9719b86c70b631e8b7553554489038cdbca9f2d
David Ramos <dave@imaginaryterrain.com>
2012-10-19 20:56:00 -0400

    Set up project for site redesign work, based on static files
    Remove unneeded Django files; add empty project based on H5BP;
    configure Compass and CodeKit.

--- kcdc3/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from userena.models import UserenaBaseProfile
-
-class ExtendedProfile(UserenaBaseProfile):
-	user = models.OneToOneField(User, unique=True)
-	phone_number = models.CharField(max_length=32, blank=True, null=True)
-	zipcode = models.CharField(max_length=5, blank=True, null=True)
-
